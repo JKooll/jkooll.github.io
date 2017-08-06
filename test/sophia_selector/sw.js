bf374bc03c5800cedc0c0ccbd33eb6eca4154887
JKooll <jerryzhao1212@gmail.com>
2017-08-06 22:41:21 +1000

    Site updated: 2017-08-06 22:41:15

--- test/sophia_selector/sw.js
+++ test/sophia_selector/sw.js
@@ -44,3 +44,22 @@ self.addEventListener('activate', function(event) {
     })
   );
 });
+
+//检查用户是否安装过此应用
+window.addEventListener('beforeinstallprompt', function(e) {
+  // beforeinstallprompt Event fired
+
+  // e.userChoice will return a Promise.
+  // For more details read: https://developers.google.com/web/fundamentals/getting-started/primers/promises
+  e.userChoice.then(function(choiceResult) {
+
+    console.log(choiceResult.outcome);
+
+    if(choiceResult.outcome == 'dismissed') {
+      console.log('User cancelled home screen install');
+    }
+    else {
+      console.log('User added to home screen');
+    }
+  });
+});
