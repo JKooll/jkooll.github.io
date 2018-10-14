a567fd3c6149d017e2c1a10850b3df04ac2059cc
JKooll <jerryzhao1212@gmail.com>
2018-10-15 09:27:10 +1000

    update

--- resume/main.js
+++ resume/main.js
@@ -9,7 +9,7 @@ const messages = {
         },
         name: 'Jerry Zhao',
         title: 'Backend Developer',
-        profile: 'Three years of PHP development experience, the technology stack is LAMP, has been able to independently develop small and medium-sized projects, and participated in relevant competitions, and achieved good results. Currently studying IT at Southern Cross University in Australia, with good teamwork skills, the ability to read English documents, and the like to research new technologies.',
+        profile: 'I am a website developer and I have three years of experience in website development, and the technology stack is LAMP. I have been able to develop small and medium-sized projects independently and participate in relevant competitions and have been made good grades. I have good teamwork ability. I am full of thirst for knowledge and am interested in technologies. I enjoy programming.',
         experience: {
             item_1: {
                 job_company: 'YUEXIANG Culture Media',
@@ -43,7 +43,7 @@ const messages = {
       },
       name: '赵树权',
       title: '后端开发者',
-      profile: '三年PHP开发经验，技术栈为LAMP，已经可以独立开发中小型项目，并参加相关比赛，取得过不错的成绩。目前在澳大利亚Southern Cross University读IT，具备很好的团队协作能力，具有阅读英文文档的能力，喜欢研究新技术。',
+      profile: '我是一名网站开发人员，我有三年的网站开发经验，技术堆栈是LAMP。 我已经能够独立开发中小型项目并参加相关比赛，并取得了不错的成绩。 我有很好的团队合作能力。 我对知识充满渴望，对技术很感兴趣。 我喜欢编程。',
       experience: {
           item_1: {
               job_company: '悦享文化传媒',
