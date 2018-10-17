087facac602aa4c2d302f563580f4dcefbe7fa2f
JKooll <jerryzhao1212@gmail.com>
2018-10-17 10:38:43 +1000

    update

--- resume/main.js
+++ resume/main.js
@@ -1,22 +1,42 @@
 const messages = {
     en: {
         labels: {
+            advantages: 'Advantages',
             experience: 'Experience',
             education: 'Education',
             skills: 'Skills',
+            activities: 'Activities',
+            adwards: 'Adwards',
             interests: 'Interests',
             reference: 'Reference'
         },
         name: 'Jerry Zhao',
         title: 'PHP Developer',
-        profile: 'I am a website developer and I have three years of experience in website development, and the technology stack is LAMP. I have been able to develop small and medium-sized projects independently and participate in relevant competitions and have been made good grades. I have good teamwork ability. I am full of thirst for knowledge and am interested in technologies. I enjoy programming.',
+        profile: 'A highly resourceful, innovative, and competent PHP developer with extensive experience in the layout, design and coding of websites specifically in PHP format. Possessing considerable knowledge of the development of web applications and scripts using PHP programming language and MySQL & SQL Server databases. Ability to complete web applications independently. Have good teamwork skills. Like programming, embrace open source, there are multiple open source projects on Github. <p>Looking for a suitable developer position with a ambitious & exciting company.</p>',
+        advantages: {
+            item_1: 'Can complete web application development independently',
+            item_2: 'Have good learning, communication, problem solving skills and strong sense of responsibility',
+            item_3: 'Have the spirit of Geek, love open source, there are many open source projects on GitHub',
+            item_4: 'Good English, accessible English documents',
+            item_5: 'Skilled collaboration, no stress writing technical documentation, project documentation'
+        },
         experience: {
             item_1: {
                 job_company: 'YUEXIANG Culture Media',
-                job_title: 'Backend Developer',
+                job_title: 'PHP Developer',
                 job_time: 'Oct. 2017 - Feb. 2018',
                 job_description: 'The main achievements of the position include: maintaining the company\'s website with other colleagues; participating in and completing the company\'s core product development; and timely solving customer problems.'
             }
+        },
+        skills: {
+            item_1: 'Web Development: PHP/TypeScript/Node',
+            item_2: 'Web Frameworks: Laravel/Slim/ThinkPHP',
+            item_3: 'Frontend Frameworks: Bootstrap/AngularJS/VueJS/Ionic',
+            item_4: 'Frontend Tools: SaSS/PhoneGap',
+            item_5: 'Data: MySQL/PDO',
+            item_6: 'Version Management, Documentation, and Automated Deployment: SVN/Git/PHPDoc/Composer',
+            item_7: 'Unit Test: PHPUnit',
+            item_8: 'Cloud and Open Platform: AWS/SAE/Wechat Application Development'
         },
         education: {
             item_1: {
@@ -31,26 +51,80 @@ const messages = {
               time: 'Sept. 2014 - Oct. 2016',
               description: 'Related courses: Introduction to Computer Science, C++, Java, Computer Network, Discrete Mathematics.'
           }
+        },
+        activities: {
+            item_1: {
+                name: 'ACM Programming Group',
+                title: 'Society',
+                time: 'Sept. 2014 - Oct. 2016',
+                description: 'Responsible for participating in various levels of programming competitions on behalf of the school, including ACM, CCPC, Lan Qiao Programming Contest and so on.'
+            }
+        },
+        adwards: {
+            item_1: {
+                name: 'The Contest of Lan Qiao Cup',
+                title: 'Second Prize',
+                time: 'March 2016',
+                description: 'Lan Qiao Cup is the largest professional computer contest in China. Since 2010, the contest has been held for nine times, once a year. Since its start, including Peking University, Tsinghua University, Fudan University, Zhejiang University and other Chinese first-class universities, more than 1,200 institutions from 31 provinces and autonomous regions in China selected outstanding students to attend the contest. '
+            },
+            item_2: {
+                name: 'China Collegiate Programming Contest',
+                title: 'Second Prize',
+                time: 'Sept. 2015',
+                description: 'China Collegiate Programming Contest (CCPC) is an international annual event hosted by the China University Student Programming Competition Association for the world\'s college students. It aims to inspire contemporary college students to use computer programming skills and skills to solve practical problems and stimulate His interest in learning algorithms and programming has fostered a sense of teamwork, innovation and challenge.'
+            },
+            item_3: {
+                name: 'Internet + College Students Innovation and Entrepreneurship Competition',
+                title: 'Silver Award',
+                time: 'Sept. 2015',
+                description: 'Stimulate the creativity of college students, cultivate the new force that creates "mass entrepreneurship, innovation"; promote the transformation of the results of the competition and the close combination of production, education and research, promote the formation of new "Internet +" business, upgrade the service economy, upgrade efficiency; lead innovation with innovation Entrepreneurship drives employment and promotes higher quality entrepreneurship and employment for college graduates.'
+            },
+            item_4: {
+                name: 'National Multimedia Courseware Competition',
+                title: 'Second Price',
+                time: 'August 2015',
+                description: 'The courseware contest will collect the courseware for teachers and information technology personnel of all levels and at all levels across the country, and invite the experts in the field of modern education technology and experts from various disciplines to form a judging panel for review. Through evaluation, a number of good works were selected to enrich the teaching content, help all educational and teaching units to improve the production and application level of multimedia courseware, improve the ability of teachers to apply information technology for curriculum integration, and promote education and teaching reform.'
+            }
         }
     },
     zh: {
       labels: {
+          advantages: '优势',
           experience: '经历',
           education: '教育',
           skills: '技能',
+          activities: '活动',
+          adwards: '奖项',
           interests: '兴趣',
           reference: '参考'
       },
       name: '赵树权',
       title: 'PHP开发工程师',
-      profile: '我是一名PHP开发人员，我有三年的网站开发经验，技术堆栈是LAMP。 我已经能够独立开发中小型项目并参加相关比赛，并取得了不错的成绩。 我有很好的团队合作能力。 我对知识充满渴望，对技术很感兴趣。 我喜欢编程。',
+      profile: '一个创新且能干的PHP开发者，在网站开发方面有丰富的经验。拥有使用Linux，Apache，MySQL和PHP开发Web应用的丰富知识。能够独立完成Web应用。拥有良好的团队协作能力。喜欢编程，拥抱开源，在Github上有多个开源项目。<p>目前正在寻找一个合适的PHP开发工程师职位。</p>',
+      advantages: {
+          item_1: '能独立完成Web应用开发',
+          item_2: '有良好的学习能力、沟通能力、解决问题能力、拥有较强的工作责任感',
+          item_3: '拥有Geek精神，热爱开源，在GitHub上有很多开源项目',
+          item_4: '英语良好，无障碍阅读英文文档',
+          item_5: '熟练协作，无压力编写技术文档、项目文档'
+      },
       experience: {
           item_1: {
               job_company: '悦享文化传媒',
-              job_title: '后端开发者',
+              job_title: 'PHP开发工程师',
               job_time: '2017.10 - 2018.2',
               job_description: '该职位的主要成就包括：与其他同事维护公司网站; 参与并完成公司的核心产品开发; 及时解决客户的问题。'
           }
+      },
+      skills: {
+          item_1: 'Web开发：PHP/TypeScript/Node',
+          item_2: 'Web框架：Laravel/Slim/ThinkPHP',
+          item_3: '前端框架：Bootstrap/AngularJS/VueJS/Ionic',
+          item_4: '前端工具：SaSS/PhoneGap',
+          item_5: '数据库相关：MySQL/PDO',
+          item_6: '版本管理、文档和自动化部署：SVN/Git/PHPDoc/Composer',
+          item_7: '单元测试：PHPUnit',
+          item_8: '云和开放平台：AWS/SAE/微信应用开发'
       },
       education: {
           item_1: {
@@ -65,6 +139,40 @@ const messages = {
             time: '2014.9 - 2016.10',
             description: '相关课程：计算机科学导论，C++，Java，计算机网络，离散数学。'
         }
+      },
+      activities: {
+          item_1: {
+              name: 'ACM编程小组',
+              title: '社团',
+              time: '2014.9 - 2016.10',
+              description: '主要负责代表学校参加各级别编程比赛，包括ACM，CCPC，蓝桥杯等。'
+          }
+      },
+      adwards: {
+          item_1: {
+              name: '蓝桥杯大赛',
+              title: '二等奖',
+              time: '2016.3',
+              description: '为推动软件开发技术的发展，促进软件专业技术人才培养，向软件行业输送具有创新能力和实践能力的高端人才，提升高校毕业生的就业竞争力，全面推动行业发展及人才培养进程，工业和信息化部人才交流中心特举办“全国软件专业人才设计与创业大赛”。'
+          },
+          item_2: {
+              name: '中国大学生程序设计大赛',
+              title: '二等奖',
+              time: '2015.9',
+              description: '中国大学生程序设计竞赛（China Collegiate Programming Contest， 简称CCPC）是由中国大学生程序设计竞赛协会主办的面向世界大学生的国际性年度赛事，旨在激励当代大学生运用计算机编程技术和技能来解决实际问题，激发其学习算法和程序设计的兴趣，培养其团队合作意识、创新能力和挑战精神。'
+          },
+          item_3: {
+              name: '首届互联网+大学生创新创业大赛',
+              title: '银奖',
+              time: '2015.9',
+              description: '激发大学生的创造力，培养造就“大众创业、万众创新”的生力军；推动赛事成果转化和产学研用紧密结合，促进“互联网+”新业态形成，服务经济提质增效升级；以创新引领创业、创业带动就业，推动高校毕业生更高质量创业就业。'
+          },
+          item_4: {
+              name: '全国多媒体课件大赛',
+              title: '二等奖',
+              time: '2015.8',
+              description: '课件大赛将在全国范围内面向各级各类院校教师和信息技术人员征集参赛课件，邀请现代教育技术领域和各学科知名专家组成评审组进行评审。通过评审，遴选出一批好的作品，丰富教学内容，帮助各教育教学单位提高多媒体课件制作及应用水平，提升广大教师应用信息技术进行课程整合的能力，推动教育教学改革。'
+          }
       }
     }
   }
@@ -85,7 +193,7 @@ const messages = {
       },
       created: function () {
           hotkeys('ctrl+s', function (e, handler) {
-            html2pdf().from(document.getElementById('resume')).save();
+            window.print();
           });
       }
   })
